9123dd38b14f6abf2ead3eb3b2776c5dcb38823a
Yahya Erdogan <yahyaerdoan@gmail.com>
2024-05-22 22:23:27 -0500

    Home, default created.

---
+++ ForestTime.UserInterfaceLayer/Controllers/Defaults/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using ForestTime.UserInterfaceLayer.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+
+namespace ForestTime.UserInterfaceLayer.Controllers.Defaults
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Default()
+        {
+            return View();
+        }
+    }
+}
